648dd2b6b623df76d820e2cf344065cc431f24b7
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-06-08 02:11:13 -0700

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    size = len(matrix[0])
-    answer = [[matrix[j][i] ** 2 for i in range(size)] for j in range(size)]
-    return answer
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
