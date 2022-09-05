d8c08b484e8020ee764f8377512d35f13e890a9f
EYOELTEKLE <eyoeltekle10@gmail.com>
2022-09-05 09:14:35 +0000

    code refraction fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2])) console.log('Missing number of occurrences');
 const number = Number(process.argv[2]);
-if (number >= 0) {
-  for (let i = 0; i < number; i++) {
-    console.log('c is fun');
-  }
+for (let i = 0; i < number; i++) {
+  console.log('c is fun');
 }
