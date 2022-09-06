18a50580d9dd7bda1c770200c92ff80f80150e26
EYOELTEKLE <eyoeltekle10@gmail.com>
2022-09-06 09:18:22 +0000

    code refraction and fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const hash = {};
-  for (const item of list) {
-    if (!(item in hash)) hash[item] = 0;
-    hash[item] += 1;
-  }
-  return hash[searchElement] === undefined ? 0 : hash[searchElement];
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
