b7a0928144eb09cfe0e0279fa4a7c935f8352e0b
EYOELTEKLE <eyoeltekle10@gmail.com>
2022-09-06 09:12:08 +0000

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const hash = {};
+  for (const item of list) {
+    if (!(item in hash)) hash[item] = 0;
+    hash[item] += 1;
+  }
+  return hash[searchElement];
+};
